da595f599ec8fa7c5e961001e9b3aa50169316b2
fuboat <fuboat@outlook.com>
2021-01-05 22:55:11 +0800

    back一些东西

--- xv6/textframe.h
+++ xv6/textframe.h
@@ -16,3 +16,13 @@ int textframe_write(struct textframe *text, char *filename);
 struct textframe *textframe_extract(struct textframe *text, int start_row, int start_col, int end_row, int end_col);
 struct textframe *textframe_delete(struct textframe *text, int start_row, int start_col, int end_row, int end_col);
 struct textframe *textframe_insert(struct textframe *text, struct textframe *in_text, int start_row, int start_col);
+void putc_to_str(struct textframe * text, int ch);
+void backspace_to_str(struct textframe * text);
+void move_to_next_char(struct textframe * text);
+void move_to_previous_char(struct textframe * text);
+void move_to_next_line(struct textframe * text);
+void move_to_last_line(struct textframe * text);
+void new_line_to_editor(struct textframe * text);
+void move_to_end(struct textframe * text);
+
+extern struct textframe * command_textframe;
